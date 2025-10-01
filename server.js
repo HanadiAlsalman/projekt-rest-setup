f6eb7be330351deb378a98ad3284e0ad18b51761
Hanadi Alsalman <alsalmanhanadi@gmail.com>
2025-10-01 10:28:22 +0200

    Lägg till express session

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 import express from 'express';
 import mysql from 'mysql2/promise';
 import dbCredentials from './db-credentials.js';
+import session from "express-session"
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+app.use(session({
+  secret: 'min-hemlighet', // en hemlig nyckel för att signera session-cookie
+  resave: false, // undviker att spara sessionen om den inte ändras
+  saveUninitialized: true, // spara en ny session som inte har blivit initialiserad
+  cookie: {secure: false} // cookie-inställningar, secure bör vara true i produktion med HTTPS
+}))
+
 
 // Skapa anslutningen direkt vid start
 const db = await mysql.createConnection(dbCredentials);
@@ -14,6 +23,17 @@ const db = await mysql.createConnection(dbCredentials);
 app.get("/", async (req, res) => {
   res.json({ message: "Servern är igång" });
 });
+
+app.get('/check-session', (req, res) => {
+  if (req.session.page_views){
+    req.session.page_views++;
+    res.send(`Du har besökt denna sida ${req.session.page_views} gånger`);
+  } else {
+    req.session.page_views = 1;
+    res.send('Välkommen till denna sida för första gången!');
+  }
+})
+
 
 app.listen(port, () => {
   console.log(`Servern körs på http://localhost:${port}`);
